bb7ebca8a1ee87d2d3ab9f70117fe25a64c1c243
Revanth <revanthrathanbonigala@gmail.com>
2024-04-21 11:49:42 +0530

    1st commit for Front End Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-angular-project';
+export class AppComponent implements OnInit {
+  title : any;
+
+  constructor(private apiService: ApiService) { }
+  ngOnInit(): void {
+    this.apiService.getHello().subscribe(
+      (response: any) => {
+        this.title = response.message;
+      },
+      (error) => {
+        console.error('Error fetching hello message:', error);
+      }
+    );
+  }
 }
